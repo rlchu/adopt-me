f79c368f2d6e580dbca6f2a17703418af314878e
rlchu <ryan.chupick@gmail.com>
2020-09-07 11:39:46 -0700

    header link to homepage && debugging tips

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Details = () => {
-  return <h1>hi!</h1>;
+const Details = (props) => {
+  return (
+    <pre>
+      <code>{JSON.stringify(props, null, 4)}</code>
+    </pre>
+  );
+  // return <h1>hi!</h1>;
 };
 
 export default Details;
