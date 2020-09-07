9dbc5ad494d6ffa7439a784dc3b57c357f81d6e7
rlchu <ryan.chupick@gmail.com>
2020-09-07 11:29:40 -0700

    routing to details in app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
         <h1>Adopt me!</h1>
-        <SearchParams></SearchParams>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
